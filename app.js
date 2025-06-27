3d1bd6c0c0e87e5d1f04076ebb5c7d294af352e3
shigenaga.k <kshigenaga5@gmail.com>
2025-06-27 11:21:32 -0700

    add: player

--- app.js
+++ app.js
@@ -7,11 +7,35 @@ const Header = (props) => {
   );
 }
 
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        name
+      </span>
+      <Score />
+    </div>
+  );
+}
+
+class Score extends React.Component {
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement">-</button>
+        <span className="counter-score">30</span>
+        <button className="counter-action increment">+</button>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header players="3" />
+        <Player />
       </div>
     );
   }
