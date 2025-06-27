3f2bce197740dae1856fd8b5928613f50dd297d1
shigenaga.k <kshigenaga5@gmail.com>
2025-06-27 11:09:27 -0700

    add: first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>test
+      </div>
+    );
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
